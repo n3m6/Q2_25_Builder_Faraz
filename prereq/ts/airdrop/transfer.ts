79009389f0e95be77aa769cdb14a28c421f392d1
Abdulla Faraz <abdulla.faraz@gmail.com>
2025-03-26 11:13:01 +0530

    empty devnet wallet

--- prereq/ts/airdrop/transfer.ts
+++ prereq/ts/airdrop/transfer.ts
@@ -7,3 +7,64 @@ import {
   sendAndConfirmTransaction,
   PublicKey,
 } from '@solana/web3.js';
+import wallet from './dev-wallet.json';
+
+// We're going to import our keypair from the wallet file const
+// public key dev-wallet.json is 6rJCyXR9DvjPdXAffj6ixaEm35WSxFLbuu1wMuPxQPgZ
+const from = Keypair.fromSecretKey(new Uint8Array(wallet));
+const connection = new Connection('https://api.devnet.solana.com');
+
+// Define our Turbin3 public key (dev net wallet we created)
+const to = new PublicKey('HfNNFfKyrrv61ymaumuBDrE113rqKRHtVYZHHLqSgx3Q');
+
+async function transfer() {
+  try {
+    const balance = await connection.getBalance(from.publicKey)
+
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: from.publicKey,
+        toPubkey: to,
+        lamports: balance,
+      })
+    );
+
+    transaction.recentBlockhash = (
+      await connection.getLatestBlockhash('confirmed')
+    ).blockhash;
+
+    transaction.feePayer = from.publicKey;
+
+    // Calculate exact fee rate to transfer entire SOL amount out of account minus fees
+    const fee = (await
+      connection.getFeeForMessage(transaction.compileMessage(),
+        'confirmed')).value || 0;
+
+    // Remove our transfer instruction to replace it
+    transaction.instructions.pop();
+
+    // Now add the instruction back with correct amount of lamports
+    transaction.add(
+      SystemProgram.transfer({
+        fromPubkey: from.publicKey,
+        toPubkey: to,
+        lamports: balance - fee,
+      })
+    );
+
+    // Sign transaction, broadcast, and confirm
+    const signature = await sendAndConfirmTransaction(
+      connection,
+      transaction,
+      [from],
+      );
+
+    console.log(
+      `Success! Check out your TX here: https://explorer.solana.com/tx/${signature}?cluster=devnet`
+    );
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
+}
+
+transfer().then(() => {});
