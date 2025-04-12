1cf4f3d1c762d35cf502fda62fd77e503143f711
Abdulla Faraz <abdulla.faraz@gmail.com>
2025-04-12 15:55:40 +0530

    adding tests

--- classes/vault-anchor/tests/vault-anchor.ts
+++ classes/vault-anchor/tests/vault-anchor.ts
@@ -1,16 +1,107 @@
+import { publicKey } from '@metaplex-foundation/umi';
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { VaultAnchor } from "../target/types/vault_anchor";
 
 describe("vault-anchor", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.VaultAnchor as Program<VaultAnchor>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const vaultState = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("state"), provider.publicKey.toBytes()],
+    program.programId
+  )[0];
+
+  const vault = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("vault"), vaultState.toBytes()],
+    program.programId
+  )[0];
+  const systemProgram = anchor.web3.SystemProgram.programId;
+
+  it("should be initialize-able", async () => {
+    const tx = await program.methods
+    .initialize()
+    .accountsPartial({
+      user: provider.wallet.publicKey,
+      vaultState,
+      vault,
+      systemProgram,
+    })
+    .rpc();
+
+    console.log(`Your transaction signature`, tx);
+    console.log(
+      'Your vault info',
+      await provider.connection.getAccountInfo(vault)
+    );
   });
+
+  it("should allow deposits of sol", async () => {
+    const tx = await program.methods
+      .deposit(new anchor.BN(2 * anchor.web3.LAMPORTS_PER_SOL))
+      .accountsPartial({
+        user: provider.wallet.publicKey,
+        vaultState,
+        vault,
+        systemProgram,
+      });
+
+      console.log(`Your transaction signature`, tx);
+      console.log(
+        'Your vault info',
+        await provider.connection.getAccountInfo(vault)
+      );
+      console.log(
+        'Your vault balance',
+        ((await provider.connection.getBalance(vault)).toString())
+      );
+  });
+
+  it('should allow withdrawals', async () => {
+    const tx = await program.methods
+      .withdraw(new anchor.BN(1 * anchor.web3.LAMPORTS_PER_SOL))
+      .accountsPartial({
+        user: provider.wallet.publicKey,
+        vaultState,
+        vault,
+        systemProgram,
+      });
+
+      console.log(`Your transaction signature`, tx);
+      console.log(
+        'Your vault info',
+        await provider.connection.getAccountInfo(vault)
+      );
+      console.log(
+        'Your vault balance',
+        ((await provider.connection.getBalance(vault)).toString())
+      );
+  });
+
+  it('should allow the vault to be closed', async () => {
+    const tx = await program.methods
+    .close()
+    .accountsPartial({
+      user: provider.wallet.publicKey,
+      vaultState,
+      vault,
+      systemProgram,
+    });
+
+    console.log(`Your transaction signature`, tx);
+    console.log(
+      'Your vault info',
+      await provider.connection.getAccountInfo(vault)
+    );
+    console.log(
+      'Your vault balance',
+      ((await provider.connection.getBalance(vault)).toString())
+    );
+  })
+
+
+
 });
